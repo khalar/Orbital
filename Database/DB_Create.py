ad8cdde24281df26f388a7614c658c8d685eb2b8
websterbei <bei.yijie.2014@vjc.sg>
2016-05-28 15:47:14 +0800

    基础框架

--- Database/DB_Create.py
+++
@@ -1,5 +0,0 @@
-from SQL_Query_Handler import *
-
-query = "CREATE TABLE student (id int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,username varchar(10) DEFAULT NULL,monday char(96) DEFAULT NULL,tuesday char(96) DEFAULT NULL,wednesday char(96) DEFAULT NULL,thursday char(96) DEFAULT NULL,friday char(96) DEFAULT NULL,saturday char(96) DEFAULT NULL,sunday char(96) DEFAULT NULL)"
-
-sql_query(query)
